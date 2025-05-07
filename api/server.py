6053ae22126176ba268624d8ac4200452af39f97
topurayhan007 <topurayhan007@gmail.com>
2025-05-07 11:21:37 +0600

    Update: All APIs done, testing

--- api/server.py
+++ api/server.py
@@ -14,6 +14,8 @@ from application_layer.services.employee_service import EmployeeService
 from application_layer.services.education_service import EducationService
 from application_layer.services.experience_service import ExperienceService
 from application_layer.classes.employee import Employee
+from application_layer.classes.education import EducationalDegree
+from application_layer.classes.experience import Experience
 
 PORT = 8000
 load_dotenv()
@@ -48,7 +50,7 @@ class Server(BaseHTTPRequestHandler):
             # http://localhost:8000/api/employees
             elif self.path == "/api/employees":
                 employees = employee_service.get_all_employee()
-                employee_dict = [employee.__dict__ for employee in employees]
+                employee_dict = [employee.__dict__ for employee in employees] if employees else []
                 self._send_response(200, "OK", {"employees": employee_dict})
             
             # http://localhost:8000/api/employees?q={input}
@@ -57,6 +59,22 @@ class Server(BaseHTTPRequestHandler):
                 employees = employee_service.search_employee(search_text)
                 employee_dict = [employee.__dict__ for employee in employees]
                 self._send_response(200, "OK", {"employees": employee_dict})
+            
+            # Education/Degrees API
+            # http://localhost:8000/api/degrees/{id}
+            elif self.path.startswith("/api/degrees/"):
+                employee_id = self.path.split("degrees/")[1]
+                degrees = education_service.search_degrees_of_an_employee(employee_id)
+                degrees_dict = [edu.__dict__ for edu in degrees] if degrees else []
+                self.self._send_response(200, "OK", {"degrees": degrees_dict})
+
+            # Experiences API
+            # http://localhost:8000/api/experiences/{id}
+            elif self.path.startswith("/api/experiences/"):
+                employee_id = self.path.split("experiences/")[1]
+                experiences = experience_service.search_experiences_of_an_employee(employee_id)
+                experiences_dict = [edu.__dict__ for edu in experiences] if experiences else []
+                self.self._send_response(200, "OK", {"experiences": experiences_dict})
                 
             else:
                 self._send_response(404, "Not Found", {"error": "Invalid route"})
@@ -75,31 +93,25 @@ class Server(BaseHTTPRequestHandler):
             input_data = None
         
         try:
+            # Employee API
+            # http://localhost:8000/api/employees
             if self.path == "/api/employees":
-                employee = Employee(
-                    input_data["_employee_id"],
-                    input_data["_name"],
-                    input_data["_date_of_birth"],
-                    input_data["_nid"],
-                    input_data["_email"],
-                    input_data["_phone_no"],
-                    input_data["_gender"],
-                    input_data["_father_name"],
-                    input_data["_mother_name"],
-                    input_data["_marital_status"],
-                    input_data["_dept"],
-                    input_data["_designation"],
-                    input_data["_nationality"],
-                    input_data["_joining_date"],
-                    input_data["_present_address"],
-                    input_data["_permanent_address"]
-                )
-
+                employee = self.__json_to_employee_obj(input_data)
                 result = employee_service.add_employee(employee)
                 self._send_response(201, "Created", {"result": result})
             
+            # Education/Degress API
+            # http://localhost:8000/api/degrees
             elif self.path == "/api/degrees":
-                result = education_service.add_degree(input_data)
+                degree = self.__json_to_degree_obj(input_data)
+                result = education_service.add_degree(degree)
+                self._send_response(201, "Created", {"result": result})
+
+            # Experiences API
+            # http://localhost:8000/api/experiences
+            elif self.path == "/api/experiences":
+                experience = self.__json_to_experience_obj(input_data)                
+                result = experience_service.add_experience(experience)
                 self._send_response(201, "Created", {"result": result})
 
             else:
@@ -108,10 +120,114 @@ class Server(BaseHTTPRequestHandler):
         except Exception as e:
             self._send_response(500, "Server Error", {"error": str(e)})
         
+    def do_PUT(self):
+        content_length = int(self.headers['Content-Length'])
+        
+        if content_length:
+            input_json = self.rfile.read(content_length).decode('utf-8')
+            input_data = json.loads(input_json)
+        else:
+            input_data = None
+        
+        try:
+            # Employee API
+            # http://localhost:8000/api/employees
+            if self.path == "/api/employees":
+                employee = self.__json_to_employee_obj(input_data)
+                result = employee_service.update_an_employee(employee)
+                self._send_response(201, "Created", {"result": result})
+            
+            # Education/Degress API
+            # http://localhost:8000/api/degrees
+            elif self.path == "/api/degrees":
+                degree = self.__json_to_degree_obj(input_data)
+                result = education_service.update_a_degree_of_an_employee(degree)
+                self._send_response(201, "Created", {"result": result})
+
+            # Experiences API
+            # http://localhost:8000/api/experiences
+            elif self.path == "/api/experiences":
+                experience = self.__json_to_experience_obj(input_data)
+                result = experience_service.update_an_experience_of_an_employee(experience)
+                self._send_response(201, "Created", {"result": result})
+
+            else:
+                self._send_response(404, "Not Found", {"error": "Invalid route"})
+
+        except Exception as e:
+            self._send_response(500, "Server Error", {"error": str(e)})
         
     
     def do_DELETE(self):
-        raise NotImplementedError
+        try:
+            # Employee API
+            # http://localhost:8000/api/degrees/{id}
+            if self.path.startswith("/api/employees/"):
+                employee_id = self.path.split("employees/")[1]
+                result = employee_service.delete_an_employee(employee_id)
+                self._send_response(200, "OK", {"result": result})
+
+            # Education/Degrees API
+            # http://localhost:8000/api/degrees/{id}
+            elif self.path.startswith("/api/degrees/"):
+                employee_id = self.path.split("degrees/")[1]
+                result = education_service.delete_a_degree_of_an_employee(employee_id)
+                self._send_response(200, "OK", {"result": result})
+
+            # Experiences API
+            # http://localhost:8000/api/experiences/{id}
+            elif self.path.startswith("/api/experiences/"):
+                employee_id = self.path.split("experiences/")[1]
+                result = experience_service.delete_an_experience_of_an_employee(employee_id)
+                self._send_response(200, "OK", {"result": result})
+
+        except Exception as e:
+            self._send_response(500, "Server Error", {"error": str(e)})
+        
+
+    def __json_to_employee_obj(self, employee) -> Employee:
+        return Employee(
+            employee["_employee_id"],
+            employee["_name"],
+            employee["_date_of_birth"],
+            employee["_nid"],
+            employee["_email"],
+            employee["_phone_no"],
+            employee["_gender"],
+            employee["_father_name"],
+            employee["_mother_name"],
+            employee["_marital_status"],
+            employee["_dept"],
+            employee["_designation"],
+            employee["_nationality"],
+            employee["_joining_date"],
+            employee["_present_address"],
+            employee["_permanent_address"]
+        )
+    
+    def __json_to_degree_obj(self, degree) -> EducationalDegree:
+        return EducationalDegree(
+            degree["_degree_id"],
+            degree["_employee_id"],
+            degree["_degree_name"],
+            degree["_institute_name"],
+            degree["_major"],
+            degree["_location"],
+            degree["_gpa"],
+            degree["_gpa_scale"],
+            degree["_year_of_passing"]
+        )
+    
+    def __json_to_experience_obj(self, experience) -> Experience:
+        return Experience(
+            experience["_experience_id"],
+            experience["_employee_id"],
+            experience["_company_name"],
+            experience["_position"],
+            experience["_joining_date"],
+            experience["_ending_date"],
+            experience["_location"]
+        )
 
 def run(server_class=HTTPServer, handler_class=Server):
     server_address = ('', PORT)
